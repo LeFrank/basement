f26bad9abbc0b19c7937da811ce34c7c740278ef
LeFrank <campbellfd@gmail.com>
2010-05-28 10:03:16 +0200

    Third Check in  added , mod rewrite and smarty template engine

--- index.php
+++ index.php
@@ -1,17 +1,52 @@
-<html>
-<head>
-    <title>
-        Here we go
-    </title>
-    <link href='http://fonts.googleapis.com/css?family=Reenie+Beanie' rel='stylesheet' type='text/css'>
-    <script style="text/css">
-        .headline1{
-            font-family: 'Reenie Beanie', arial, serif;
-        }
-    </script>
-</head>
-<body>
-    <span class="headline1">This is the most awesomest! Font ever.</span>
-</body>
+<?php
+error_reporting(E_ALL ^ E_NOTICE ^ E_WARNING ^ E_COMPILE_WARNING);
+if(!ob_start("ob_gzhandler"))
+	ob_start();
+require_once("./config/initialize.php");
 
-</html>
+$ad_data_array = $_REQUEST;
+
+
+if (isset($_GET['module'])) {
+      $module = $_GET['module'];
+      if (isset($_GET['event'])) {
+          $event = $_GET['event'];
+      } else {
+          $event = '__default';
+      }
+
+      if (isset($_GET['class'])) {
+          $class = $_GET['class'];
+      } else {
+          $class = $module."_controller";
+      }
+      $classFile = DOC_ROOT.'/module/'.$module.'/'.$class.'.php';
+      if (file_exists($classFile)) {
+          require_once($classFile);
+          if (class_exists($class)) {
+              try {
+                  $instance = new $class();
+                  if (!is_object($instance)) {
+                      die("Requested module is not a valid framework module!");
+                  }
+                  $instance->moduleName = $module;
+                      try {
+                          $result = $instance->$event();
+                      } catch (Exception $error) {
+                          die($error->getMessage());
+                      }
+              } catch (Exception $error) {
+                  die($error->getMessage());
+              }
+          } else {
+              die("An valid module for your request was not found");
+          }
+      } else {
+          die("Could not find: $classFile");
+      }
+  } else {
+      header('Location: http://'.$_SERVER['SERVER_NAME'].'/?module=page') ;
+  }
+ob_end_flush();
+exit;
+?>
