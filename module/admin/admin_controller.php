b332b573949c35cac0b4cdacf895c9c0378d277c
LeFrank <campbellfd@gmail.com>
2010-08-26 01:10:48 +0200

    25082010 the blog :) ... gallery

---
+++ module/admin/admin_controller.php
@@ -0,0 +1,19 @@
+<?php
+/* 
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+class admin_controller extends global_controller{
+
+    public function admin_controller(){
+        
+    }
+
+    public function __default(){
+
+    }
+
+    
+}
