4b8f069b6d47808e28abeae4b120dad84d0b64ae
LeFrank <campbellfd@gmail.com>
2010-08-26 01:12:25 +0200

    ooo

--- module/page_block/page_block_controller.php
+++ module/page_block/page_block_controller.php
@@ -10,20 +10,24 @@
  * @author francois
  */
 include_once(DOC_ROOT."module/page_block/page_block_model.php");
+include_once(DOC_ROOT."module/page_block/page_block_view.php");
 
 class page_block_controller extends global_controller{
 
     var $pbm = null;
 
     function page_block_controller(){
-        $this->pdb = new page_block_model();
+        $this->pbm = new page_block_model();
+        $this->pbv = new page_block_view();
     }
 
     function get_page_block($page_id){
 //        if($page_id){
             //get all page_blocks from the db for this page
-            $page_block_arr = $this->pdb->get_page_block($page_id);
-            $page_block_arr = array("status" => 1 , "msg"=>"" , "value" => $page_block_arr );
+            $page_block_arr = $this->pbm->get_page_block($page_id);
+            $page_block_data = $this->pbv->render_page_blocks($page_block_arr);
+            $page_block_arr = array("status" => 1 , "msg"=>"" , "value" => $page_block_data );
+
 //        }else{
 //            //TODO: return meanigfull well formatted array error
 //            $page_block_arr = "error";
